98b3940684158d1e612b9d12f13a3624002335fb
xyc <1365346020@qq.com>
2019-08-09 20:46:03 +0800

    zixun

--- resources/views/information/edit.blade.php
+++ resources/views/information/edit.blade.php
@@ -2,35 +2,39 @@
 @section('title', 'Laravel学院')
 
 @section('content')
-    <form class="layui-form" action="/formation/add_do" method="post">
+    <form class="layui-form" action="/formation/edit" method="post">
+        <input type="hidden" value="{{$info->info_id}}" name="info_id">
         <div class="layui-form-item">
             <label class="layui-form-label">资讯标题</label>
             <div class="layui-input-inline">
-                <input type="text" name="info_title" required  lay-verify="required" placeholder="请输入标题" autocomplete="on" class="layui-input">
+                <input type="text" name="info_title" required value="{{$info->info_title}}" lay-verify="required" placeholder="请输入标题" autocomplete="on" class="layui-input">
             </div>
         </div>
 
         <div class="layui-form-item layui-form-text">
             <label class="layui-form-label">资讯介绍</label>
             <div class="layui-input-inline">
-                <textarea name="info_desc" placeholder="请输入介绍内容" class="layui-textarea"></textarea>
+                <textarea name="info_desc" placeholder="请输入介绍内容" class="layui-textarea">{{$info->info_desc}}</textarea>
             </div>
         </div>
 
         <div class="layui-form-item layui-form-text">
             <label class="layui-form-label">资讯详情</label>
             <div class="layui-input-inline">
-                <textarea name="info_detail" placeholder="请输入详情内容" class="layui-textarea"></textarea>
+                <textarea name="info_detail" placeholder="请输入详情内容" class="layui-textarea">{{$info->info_detail}}</textarea>
             </div>
         </div>
 
         <div class="layui-form-item">
             <label class="layui-form-label">资讯分类</label>
             <div class="layui-input-inline">
-                <select name="c_id" lay-verify="required">
-                        <option value=""></option>
+                <select name="info_cate_id" lay-verify="required">
                     @foreach($res as $k=>$v)
-                        <option value="{{$v->info_cate_id}}">{{$v->info_name}}</option>
+                        @if($info->info_cate_id == $v->info_cate_id)
+                            <option value="{{$v->info_cate_id}}">{{$v->info_name}}</option>
+                        @else
+                            <option value="{{$v->info_cate_id}}">{{$v->info_name}}</option>
+                        @endif
                     @endforeach
                 </select>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,21 @@ Route::get('/mouke_admin',function(){
     return view('admin.index');
 });
 #资讯添加
-Route::get('/formation/add',function(){
-    return view('information.add');
-});
+Route::get('/formation/add','Zi\FormationController@add');
+#资讯添加执行
+Route::post('/formation/add_do','Zi\FormationController@add_do');
 #资讯列表
 Route::get('/formation/list','Zi\FormationController@list');
+#删除
+Route::post('/formation/del','Zi\FormationController@del');
+#修改
+Route::get('/formation/update/{id}','Zi\FormationController@update');
+#修改执行
+Route::post('/formation/edit','Zi\FormationController@edit');
 
 Route::get('/index','Index\IndexController@index');
+
+
 
 #分类资讯添加
 Route::get('/cate_formation/add',function(){
@@ -36,3 +44,5 @@ Route::get('/cate_formation/add',function(){
 Route::post('/cate_do','Zi\FormationController@cate_do');
 #分类资讯列表
 Route::get('/cate_formation/list','Zi\FormationController@cate_list');
+#分类删除
+Route::get('/cate_del/{id}','Zi\FormationController@cate_del');
